4a7c48adf60659826a7b1457d09855d49ca3676e
sudaknees <41879746+sudaknees@users.noreply.github.com>
2020-06-28 07:03:30 -0400

    another js

--- our-team/xmlparser.js
+++ our-team/xmlparser.js
@@ -11,11 +11,27 @@ $(document).ready(function()
 function parseXml(xml)
 {
   $("#dirlist").html();
-  $(xml).find("director").each(function()
+  $(xml).find("director").eq(0)
   {
-    $(".pername").html($(this).find("name").text());
-	$(".perpos").html($(this).find("position").text());
-	$("h3").html($(this).find("age").text()+', '+$(this).find("location").text()+', '+$(this).find("pronouns").text()+', ');
-	$("p").html($(this).find("bio").text());
-  });  
+    $(".pername:eq(0)").html($(this).find("name").text());
+	$(".perpos:eq(0)").html($(this).find("position").text());
+	$("h3:eq(0)").html($(this).find("age").text()+', '+$(this).find("location").text()+', '+$(this).find("pronouns").text()+', ');
+	$("p:eq(0)").html($(this).find("bio").text());
+  };  
+	
+	$(xml).find("director").eq(0)
+  {
+    $(".pername:eq(0)").html($(this).find("name").text());
+	$(".perpos:eq(0)").html($(this).find("position").text());
+	$("h3:eq(0)").html($(this).find("age").text()+', '+$(this).find("location").text()+', '+$(this).find("pronouns").text()+', ');
+	$("p:eq(0)").html($(this).find("bio").text());
+  }; 
+	
+	$(xml).find("director").eq(2)
+  {
+    $(".pername:eq(2)").html($(this).find("name").text());
+	$(".perpos:eq(2)").html($(this).find("position").text());
+	$("h3:eq(2)").html($(this).find("age").text()+', '+$(this).find("location").text()+', '+$(this).find("pronouns").text()+', ');
+	$("p:eq(2)").html($(this).find("bio").text());
+  }; 
 }
