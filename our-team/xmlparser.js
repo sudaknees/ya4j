82775da53ad7d3c487537f7d7bab01349c185bf9
sudaknees <41879746+sudaknees@users.noreply.github.com>
2020-06-28 05:21:51 -0400

    first push

---
+++ our-team/xmlparser.js
@@ -0,0 +1,18 @@
+$(document).ready(function()
+{
+  $.ajax({
+    type: "GET",
+    url: "teammembers.xml",
+    dataType: "xml",
+    success: parseXml
+  });
+});
+
+function parseXml(xml)
+{
+  $("#dirlist").html("<ul id='content' data-role='listview' data-inset='true'></ul>");
+  $(xml).find("director").each(function()
+  {
+    $("#content").append("<table><tr><td><img src='"+$(this).find("imgurl").text()+"'/></td><td><h2>"+$(this).find("name").text()+"</h2><br><h2>"+$(this).find("position").text()+"</h2><p>"+$(this).find("bio").text()+"</p></td>");
+  });  
+}
