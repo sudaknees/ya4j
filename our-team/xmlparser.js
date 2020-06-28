6925279de2c8e46a65d9bc797fbd22f791ec9899
sudaknees <41879746+sudaknees@users.noreply.github.com>
2020-06-28 06:44:45 -0400

    try new js

--- our-team/xmlparser.js
+++ our-team/xmlparser.js
@@ -10,9 +10,12 @@ $(document).ready(function()
 
 function parseXml(xml)
 {
-  $("#dirlist").html("<ul id='content' data-role='listview' data-inset='true'></ul>");
+  $("#dirlist").html();
   $(xml).find("director").each(function()
   {
-    $("#content").append("<table><tr><td><img src='"+$(this).find("imgurl").text()+"'/></td><td><h2 class='nametxt'>"+$(this).find("name").text()+"</h2 class='postext'><h2>"+$(this).find("position").text()+"</h2><h3 class='perinfo'>"+$(this).find("age").text()+", "+$(this).find("location").text()+", "+$(this).find("pronouns").text()+"</h3><p>"+$(this).find("bio").text()+"</p></td>");
+    $(".pername").html($(this).find("name").text());
+	$(".perpos").html($(this).find("position").text());
+	$("h3").html($(this).find("age").text()+', '+$(this).find("location").text()+', '+$(this).find("pronouns").text()+', ');
+	$("p").html($(this).find("bio").text());
   });  
 }
