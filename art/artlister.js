4a91be1656c7d7f55959b7bb4f64c9510489c411
sudaknees <41879746+sudaknees@users.noreply.github.com>
2020-06-29 19:40:43 -0400

    add new art

--- art/artlister.js
+++ art/artlister.js
@@ -12,7 +12,7 @@ function parseXml(xml)
 {
 	$("#container").html();
 	$(xml).find("artwork").each(function(){
-		$("#container").append("<div class='listing grid-sizer "+$(this).find("segment").text()+"'><a href='"+$(this).find("shopurl").text()+" target='_blank'><img src='"+$(this).find("imageurl").text()+"'/><br /><h3 class='arttitle'>"+$(this).find("title").text()+"</h3></a><h4 class='artinfo'>"+$(this).find("artist").text()+"</h4><p>Funds to "+$(this).find("charity").text()+"</p></div>");
+		$("#container").append("<div class='listing grid-sizer "+$(this).find("segment").text()+"'><a href='"+$(this).find("shopurl").text()+"' target='_blank'><img src='"+$(this).find("imageurl").text()+"'/><br /><h3 class='arttitle'>"+$(this).find("title").text()+"</h3></a><h4 class='artinfo'>"+$(this).find("artist").text()+"</h4><p>Funds to "+$(this).find("charity").text()+"</p></div>");
 	});
 }
 
