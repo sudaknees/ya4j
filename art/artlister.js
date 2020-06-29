5ed01118f295146eabe69c91c08996134d8a4d3b
sudaknees <41879746+sudaknees@users.noreply.github.com>
2020-06-29 19:08:55 -0400

    art again?

--- art/artlister.js
+++ art/artlister.js
@@ -12,17 +12,18 @@ function parseXml(xml)
 {
 	$("#container").html();
 	$(xml).find("artwork").each(function(){
-		$("#container").append("<div class='listing "+$(this).find("segment").text()+"'><img src='"+$(this).find("imageurl").text()+"'/><br /><a href='"+$(this).find("shopurl").text()+" target='_blank'><h3 class='arttitle'>"+$(this).find("title").text()+"</h3></a><h4 class='artinfo'>"+$(this).find("artist").text()+"</h4><p>Funds to "+$(this).find("charity").text()+"</p></div>");
-		var $grid = $('#container').isotope({
-			itemSelector: '.listing',
-			percentPosition: true,
-			masonry: {
-		  		columnWidth: '.grid-sizer',
-		  		gutter: 20}
-		});
-		$grid.imagesLoaded().progress( function() {
-		$grid.isotope('layout');
-		});
-	})
+		$("#container").append("<div class='listing "+$(this).find("segment").text()+"'><a href='"+$(this).find("shopurl").text()+" target='_blank'><img src='"+$(this).find("imageurl").text()+"'/><br /><h3 class='arttitle'>"+$(this).find("title").text()+"</h3></a><h4 class='artinfo'>"+$(this).find("artist").text()+"</h4><p>Funds to "+$(this).find("charity").text()+"</p></div>");
+	});
+	var $grid = $('#container').isotope({
+		itemSelector: '.listing',
+		percentPosition: true,
+		masonry: {
+		  	columnWidth: '.grid-sizer',
+		  	gutter: 20}
+	});
+	$grid.imagesLoaded().progress( function() {
+	$grid.isotope('layout');
+		
+	});
 }
 
