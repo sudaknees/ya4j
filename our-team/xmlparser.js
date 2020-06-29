5305c4fd2d3bd12fc8d4a0964512e0560afd19a3
sudaknees <41879746+sudaknees@users.noreply.github.com>
2020-06-29 15:21:58 -0400

    Update xmlparser.js

--- our-team/xmlparser.js
+++ our-team/xmlparser.js
@@ -10,9 +10,9 @@ $(document).ready(function()
 
 function parseXml(xml)
 {
-   $("#dirlist").html("<ul id='content' data-role='listview' data-inset='true'></ul>");
+   $("#dirlist").html();
   $(xml).find("director").each(function()
   {
-    $("#content").append("<div class='personal'><div class='imgtd'><img src='"+$(this).find("imgurl").text()+"'/></div><div class=''personalbio'><h2 class='nametxt'>"+$(this).find("name").text()+"</h2><h3 class='postext'>"+$(this).find("position").text()+"</h3><h4 class='perinfo'>"+$(this).find("age").text()+", "+$(this).find("location").text()+", "+$(this).find("pronouns").text()+"</h4><p>"+$(this).find("bio").text()+"</p></div></div><hr />");
+    $("#dirlist").append("<div class='personal'><div class='imgtd'><img src='"+$(this).find("imgurl").text()+"'/></div><div class=''personalbio'><h2 class='nametxt'>"+$(this).find("name").text()+"</h2><h3 class='postext'>"+$(this).find("position").text()+"</h3><h4 class='perinfo'>"+$(this).find("age").text()+", "+$(this).find("location").text()+", "+$(this).find("pronouns").text()+"</h4><p>"+$(this).find("bio").text()+"</p></div></div><hr />");
   });  
 }
