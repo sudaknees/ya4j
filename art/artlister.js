3d5ac2c73fc7bb16796dd17bb277d36b97bd4712
sudaknees <41879746+sudaknees@users.noreply.github.com>
2020-06-29 18:59:16 -0400

    fix art again

--- art/artlister.js
+++ art/artlister.js
@@ -12,7 +12,7 @@ function parseXml(xml)
 {
 	$("#container").html();
 	$(xml).find("artwork").each(function(){
-		$("#container").append("<div class='listing "+$(this).find("segment").text()+"'><img src='"+$(this).find("imagegurl").text()+"'/><br /><a href='"+$(this).find("shopurl").text()+" target='_blank'><h3 class='arttitle'>"+$(this).find("title").text()+"</h3></a><h4 class='artinfo'>"+$(this).find("artist").text()+"</h4><p>Funds to "+$(this).find("charity").text()+"</p></div>");
+		$("#container").append("<div class='listing "+$(this).find("segment").text()+"'><img src='"+$(this).find("imageurl").text()+"'/><br /><a href='"+$(this).find("shopurl").text()+" target='_blank'><h3 class='arttitle'>"+$(this).find("title").text()+"</h3></a><h4 class='artinfo'>"+$(this).find("artist").text()+"</h4><p>Funds to "+$(this).find("charity").text()+"</p></div>");
 		var $grid = $('#container').isotope({
 			itemSelector: '.listing',
 			percentPosition: true,
