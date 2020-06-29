1d24c082219659f4e581fab20a4cd89189764d56
sudaknees <41879746+sudaknees@users.noreply.github.com>
2020-06-29 15:24:36 -0400

    yay centered

--- our-team/xmlparser.js
+++ our-team/xmlparser.js
@@ -13,6 +13,6 @@ function parseXml(xml)
    $("#dirlist").html();
   $(xml).find("director").each(function()
   {
-    $("#dirlist").append("<div class='personal'><div class='imgtd'><img src='"+$(this).find("imgurl").text()+"'/></div><div class=''personalbio'><h2 class='nametxt'>"+$(this).find("name").text()+"</h2><h3 class='postext'>"+$(this).find("position").text()+"</h3><h4 class='perinfo'>"+$(this).find("age").text()+", "+$(this).find("location").text()+", "+$(this).find("pronouns").text()+"</h4><p>"+$(this).find("bio").text()+"</p></div></div><hr />");
+    $("#dirlist").append("<div class='personal'><div class='imgtd'><img src='"+$(this).find("imgurl").text()+"'/></div><div class=''personalbio'><h2 class='nametxt'>"+$(this).find("name").text()+"</h2><h3 class='postext'>"+$(this).find("position").text()+"</h3><h4 class='perinfo'>"+$(this).find("age").text()+", "+$(this).find("location").text()+", "+$(this).find("pronouns").text()+"</h4><p>"+$(this).find("bio").text()+"</p></div></div>");
   });  
 }
