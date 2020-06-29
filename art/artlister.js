781616da725b666402630cc2c4fb67c145d1f547
sudaknees <41879746+sudaknees@users.noreply.github.com>
2020-06-29 18:45:37 -0400

    fix js

--- art/artlister.js
+++ art/artlister.js
@@ -11,7 +11,7 @@ $(document).ready(function()
 function parseXml(xml)
 {
    $("#container").html();
-  $(xml).find("director").each(function()
+  $(xml).find("artwork").each(function()
   {
     $("#container").append("<div class='listing "+$(this).find("segment").text()+"'><img src='"+$(this).find("imagegurl").text()+"'/><br /><a href='"+$(this).find("shopurl").text()+" target='_blank'><h3 class='arttitle'>"+$(this).find("title").text()+"</h3><h4 class='artinfo'>"+$(this).find("artist").text()+"</h4><p>Funds to"+$(this).find("charity").text()+"</p></div>");
   });  
