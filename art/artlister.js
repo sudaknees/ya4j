71a59cfaeefeb6937a6ec27279a3828495a2cffb
sudaknees <41879746+sudaknees@users.noreply.github.com>
2020-06-30 03:38:37 -0400

    cleanin things up!

--- art/artlister.js
+++ art/artlister.js
@@ -13,7 +13,7 @@ function parseXml(xml)
 {
 	$("gridthingy").html();
 	$(xml).find("artwork").each(function(){
-		$("#gridthingy").append("<div class='listing grid-sizer "+$(this).find("segment").text()+"'><a href='"+$(this).find("shopurl").text()+"' target='_blank'><img src='artfiles/"+$(this).find("imageurl").text()+"'/><br /><h3 class='arttitle'>"+$(this).find("title").text()+"</h3></a><div class='igflexbox'><img src='iglogo.svg' /><a href='https://instagr.am/"+$(this).find("artist").text()+"' target='_blank'><h4 class='artinfo'>@"+$(this).find("artist").text()+"</h4></a></div><p>Funds to "+$(this).find("charity").text()+"</p></div>");
+		$("#gridthingy").append("<div class='listing grid-sizer "+$(this).find("segment").text()+"'><a href='"+$(this).find("shopurl").text()+"' target='_blank'><img src='artfiles/"+$(this).find("imageurl").text()+"'/><br /><h4 class='arttitle'>"+$(this).find("title").text()+"</h4></a><a href='https://instagr.am/"+$(this).find("artist").text()+"' target='_blank'><h5 class='artinfo'>@"+$(this).find("artist").text()+"</h5></a><p>Funds to "+$(this).find("charity").text()+"</p></div>");
 	});
 }
 				  
