0dbb1274ae0c8f84091cf76af9245f329fac73af
sudaknees <41879746+sudaknees@users.noreply.github.com>
2020-07-01 00:11:55 -0400

    Update artlister.js

--- art/artlister.js
+++ art/artlister.js
@@ -1,22 +1,21 @@
-$(document).ready(function()
-{
-  $.ajax({
-    type: "GET",
-    url: "artworks.xml",
-    dataType: "xml",
-    success: parseXml,
-	complete: runisotope
-  });
+$(document).ready(function() {
+	$.ajax({
+		type: "GET",
+		url: "artworks.xml",
+		dataType: "xml",
+		success: parseXml,
+		complete: runisotope
+	});
 });
 
 function parseXml(xml)
 {
 	$("gridthingy").html();
-	$(xml).find("artwork").each(function(){
-		$("#gridthingy").append("<div class='listing grid-sizer "+$(this).find("segment").text()+"'><a href='"+$(this).find("shopurl").text()+"' target='_blank'><img src='artfiles/"+$(this).find("imageurl").text()+"'/><br /><h4 class='arttitle'>"+$(this).find("title").text()+"</h4></a><a href='https://instagr.am/"+$(this).find("artist").text()+"' target='_blank'><h5 class='artinfo'>@"+$(this).find("artist").text()+"</h5></a><p>Funds to "+$(this).find("charity").text()+"</p></div>");
+	$(xml).find("artwork").each(function() {
+		$("#gridthingy").append("<div class='listing grid-sizer " + $(this).find("segment").text()+ "'><a href='" + $(this).find("shopurl").text() + "' target='_blank'><img src='artfiles/" + $(this).find("imageurl").text() + "'/><br /><h4 class='arttitle'>" + $(this).find("title").text() + "</h4></a><a href='https://instagr.am/" + $(this).find("artist").text() + "' target='_blank'><h5 class='artinfo'>@" + $(this).find("artist").text() + "</h5></a><p>Funds to " + $(this).find("charity").text() + "</p></div>");
 	});
 }
-				  
+
 function runisotope()
 {
 	var $grid = $('#gridthingy').isotope({
@@ -30,19 +29,19 @@ function runisotope()
 		getSortData: {
 			name: function( itemElem ) {
 				var name = $( itemElem ).find('.arttitle').text();
-      			return name.toLowerCase();
+				return name.toLowerCase();
 			},
 			nameb: function( itemElem2 ) {
-      			var nameb = $( itemElem2 ).find('.arttitle').text();
-      			return nameb.toLowerCase();
+				var nameb = $( itemElem2 ).find('.arttitle').text();
+				return nameb.toLowerCase();
 			},
 			person: function( itemElem3 ) {
-      			var person = $( itemElem3 ).find('.artinfo').text();
-      			return person.toLowerCase();
+				var person = $( itemElem3 ).find('.artinfo').text();
+				return person.toLowerCase();
 			},
 			personb: function( itemElem4 ) {
-      			var personb = $( itemElem4 ).find('.artinfo').text();
-      			return personb.toLowerCase();
+				var personb = $( itemElem4 ).find('.artinfo').text();
+				return personb.toLowerCase();
 			}
 		},
 		sortAscending: {
